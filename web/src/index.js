032e87c27d572f36d9a02186d9972009d4ee1b9c
Kendsdavs <kendra.wieck@gmail.com>
2016-11-22 14:33:51 -0500

    step 1 - app - home - about

--- web/src/index.js
+++ web/src/index.js
@@ -2,6 +2,6 @@ const React = require('react')
 
 const ReactDOM = require('react-dom')
 
-const App = () => <h1>Hello, React</h1>
+const App = require('./App')
 
 ReactDOM.render(<App />, document.getElementById('root'))
